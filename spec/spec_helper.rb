9246f54d572912d6c3d6ff316209cd2aedeaabfa
Patrick Metcalfe <git@patrickmetcalfe.com>
2014-10-12 02:40:40 -0500

    Changed Rakefile config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
 require "coloral"
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
