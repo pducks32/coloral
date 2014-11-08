5f83bee28063b7ec16c8c8b4c69b084b11bf8ea2
Patrick Metcalfe <git@patrickmetcalfe.com>
2014-11-08 16:29:52 -0600

    Collar now delegates to Color when "for || from"
    This allow collar to not have to worry about what color models are
    supported and keeps it from duplicating Color’s functionality

--- lib/coloral.rb
+++ lib/coloral.rb
@@ -2,18 +2,16 @@ require "coloral/version"
 
 module Coloral
 
-  def self.from_hex(hex_string)
-    Color.from_hex(hex_string)
+  def self.method_missing(meth, *args, &block)
+    if meth.to_s.start_with?("from", "for") && Color.respond_to?(meth)
+      Color.public_send(meth, *args)
+    else
+      super
+    end
   end
 
-  def self.from_rgb(a_red=nil, a_green=nil, a_blue=nil, r: a_red, g: a_green, b: a_blue, red: r, green: g, blue: b)
-    raise ArgumentError if [red, blue, green].any?(&:nil?)
-    Color.from_rgb(red, blue, green)
-  end
-
-  def self.from_hsl(a_hue=nil, a_saturation=nil, a_lightness=nil, h: a_hue, s: a_saturation, l: a_lightness, hue: h, saturation: s, lightness: l)
-    raise ArgumentError if [hue, saturation, lightness].any?(&:nil?)
-    Color.from_hsl(hue, saturation, lightness)
+  def respond_to_missing?(meth, include_private = false)
+    meth.to_s.start_with?("from", "for") && Color.respond_to?(meth) || super
   end
 
   class Color
@@ -23,11 +21,13 @@ module Coloral
       Color.new
     end
 
-    def self.from_rgb(a_red, a_green, a_blue, r: a_red, g: a_green, b: a_blue, red: r, green: g, blue: b)
+    def self.from_rgb(a_red = nil, a_green = nil, a_blue = nil, r: a_red, g: a_green, b: a_blue, red: r, green: g, blue: b)
+      raise ArgumentError if [red, blue, green].any?(&:nil?)
       Color.new
     end
 
-    def self.from_hsl(a_hue, a_saturation, a_lightness, h: a_hue, s: a_saturation, l: a_lightness, hue: h, saturation: s, lightness: l)
+    def self.from_hsl(a_hue = nil, a_saturation = nil, a_lightness = nil, h: a_hue, s: a_saturation, l: a_lightness, hue: h, saturation: s, lightness: l)
+      raise ArgumentError if [hue, saturation, lightness].any?(&:nil?)
       Color.new
     end
 
