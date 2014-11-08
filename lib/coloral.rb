c42b91da873be8ae28f5ff5e0ad03bc8c8de0ee3
Patrick Metcalfe <git@patrickmetcalfe.com>
2014-11-08 15:53:50 -0600

    Added "from_" for hsl, rgb, and hex

--- lib/coloral.rb
+++ lib/coloral.rb
@@ -1,8 +1,19 @@
 require "coloral/version"
 
 module Coloral
+
   def self.from_hex(hex_string)
     Color.from_hex(hex_string)
+  end
+
+  def self.from_rgb(a_red=nil, a_green=nil, a_blue=nil, r: a_red, g: a_green, b: a_blue, red: r, green: g, blue: b)
+    raise ArgumentError if [red, blue, green].any?(&:nil?)
+    Color.from_rgb(red, blue, green)
+  end
+
+  def self.from_hsl(a_hue=nil, a_saturation=nil, a_lightness=nil, h: a_hue, s: a_saturation, l: a_lightness, hue: h, saturation: s, lightness: l)
+    raise ArgumentError if [hue, saturation, lightness].any?(&:nil?)
+    Color.from_hsl(hue, saturation, lightness)
   end
 
   class Color
@@ -11,5 +22,14 @@ module Coloral
     def self.from_hex(hex_string)
       Color.new
     end
+
+    def self.from_rgb(a_red, a_green, a_blue, r: a_red, g: a_green, b: a_blue, red: r, green: g, blue: b)
+      Color.new
+    end
+
+    def self.from_hsl(a_hue, a_saturation, a_lightness, h: a_hue, s: a_saturation, l: a_lightness, hue: h, saturation: s, lightness: l)
+      Color.new
+    end
+
   end
 end
--- spec/coloral_spec.rb
+++ spec/coloral_spec.rb
@@ -2,10 +2,74 @@ require "spec_helper"
 
 module Coloral
   describe Coloral do
+
     describe "::from_hex" do
-      it "creates color using hex" do
-        expect(Coloral.from_hex("#00FF00")).to be_a Color
+      context "with no parameters" do
+        it "raises ArgumentError" do
+          expect{ Coloral.from_hex() }.to raise_error ArgumentError
+        end
+      end
+      context "when # is in front" do
+        it "returns a color" do
+          expect(Coloral.from_hex("#00FF00")).to be_a Color
+        end
+      end
+      context "when # is not in front" do
+        it "returns a color" do
+          expect(Coloral.from_hex("00FF00")).to be_a Color
+        end
       end
     end
+
+    describe "::from_hsl" do
+      context "with no parameters" do
+        it "raises ArgumentError" do
+          expect{ Coloral.from_hsl() }.to raise_error ArgumentError
+        end
+      end
+      context "with no special syntax" do
+        it "returns a color" do
+          expect(Coloral.from_hsl(175, 50, 85)).to be_a Color
+        end
+      end
+      context "with hsl hash" do
+        it "returns a color" do
+          hash = { h: 175, s: 50, l: 85 }
+          expect(Coloral.from_hsl(hash)).to be_a Color
+        end
+      end
+      context "with hue saturation lightness hash" do
+        it "returns a color" do
+          hash = { hue: 175, saturation: 50, lightness: 85 }
+          expect(Coloral.from_hsl(hash)).to be_a Color
+        end
+      end
+    end
+
+    describe "::from_rgb" do
+      context "with no parameters" do
+        it "raises ArgumentError" do
+          expect{ Coloral.from_hsl() }.to raise_error ArgumentError
+        end
+      end
+      context "with no special syntax" do
+        it "returns a color" do
+          expect(Coloral.from_rgb(200, 180, 90)).to be_a Color
+        end
+      end
+      context "with hsl hash" do
+        it "returns a color" do
+          hash = { r: 200, g: 180, b: 90 }
+          expect(Coloral.from_rgb(hash)).to be_a Color
+        end
+      end
+      context "with hue saturation lightness hash" do
+        it "returns a color" do
+          hash = { red: 200, green: 180, blue: 90 }
+          expect(Coloral.from_rgb(hash)).to be_a Color
+        end
+      end
+    end
+
   end
 end
